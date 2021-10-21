64f148d0debaff6907576e610d2a59b82cf8c3f7
rfcl <leon@epzsecurity.com>
2021-10-21 19:11:07 +1100

    Bugfixes and Placeholders
    A few bugfixes and added the beginnings of additional features.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ async function uploadSample(sampleInput) {
     try {
         let r = await fetch('/upload',
             { method: "POST", body: formData, signal: ctrl.signal }).then(response => {
-                console.log(response);
                 if (response.ok) {
                     sampleResponse = response.clone().json();
                     sampleResponse.then(function (r) {
@@ -28,14 +27,26 @@ async function uploadSample(sampleInput) {
 }
 
 function showAnalysis(sampleResults) {
+    $('#basic-analysis-table').DataTable().clear();
+    $('#basic-analysis-table').DataTable().destroy();
+    $('#macro-analysis-table').DataTable().clear();
+    $('#macro-analysis-table').DataTable().destroy();
+    document.getElementById('codeReview').innerText = "";
+    
+
     basic_analysis_table = $('#basic-analysis-table').DataTable({
         "data": sampleResults['analysis'],
         "columns":  [
             { "data":"description", "width":"75%"},
             { "data": "keyword" },
             { "data": "type" }
-                    ]
+        ],
+        "pageLength": 17
         });
+
+    for (macro_id in sampleResults['macros']) {
+        sampleResults['macros'][macro_id]['code'] = "<textarea style='display: none;'>" + sampleResults['macros'][macro_id]['code'] + "</textarea>"
+    }
     
     macro_analysis_table = $('#macro-analysis-table').DataTable({
         "data": sampleResults['macros'],
@@ -43,7 +54,7 @@ function showAnalysis(sampleResults) {
             { "data": "vba_filename" , "width":"30%"},
             { "data": "subfilename" },
             { "data": "ole_stream" },
-            {"data": "code"}
+            { "data": "code"}
         ]
     });
 
@@ -54,15 +65,15 @@ function showAnalysis(sampleResults) {
     macro_table_rows = document.getElementById('macro-analysis-table').children[1].children;
     for (mr in macro_table_rows) {
         try {
-            macroCode = macro_table_rows[mr].children[3].innerText;
-            macro_table_rows[mr].children[3].innerHTML = "<a onClick='showCode(this)' style='text-decoration:underline; cursor: pointer;'>Click to review code<p style='display: none;'>" + macroCode + "</p></a>";
+            macroCode = macro_table_rows[mr].children[3].innerHTML;
+            macro_table_rows[mr].children[3].innerHTML = "<a onClick='showCode(this)' style='text-decoration:underline; cursor: pointer;'>Click to review code" + macroCode + "</a>";
         } catch {
             continue
         }
             }
 
     for (el in allAnalysisElements) {
-        document.getElementsByClassName('analysis-results')[el].style += "display:block; margin-top: 2em; width: 49%; float: left;";
+        document.getElementsByClassName('analysis-results')[el].style += "display:block; margin-top: 2em; width: 49%; float: left; ";
     }
 
     document.getElementById('macro-analysis-card').style ="display: block; margin-top: 2em; width: 49%; float: right;";
--- olevba_web.go
+++ olevba_web.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -50,6 +55,14 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	exPath := filepath.Dir(ex)
 	fmt.Println(exPath + "//uploads//" + handler.Filename)
 
+	md5Hash, err := hash_file(exPath+"//uploads//"+handler.Filename, "md5")
+	sha1Hash, err := hash_file(exPath+"//uploads//"+handler.Filename, "sha1")
+	sha256Hash, err := hash_file(exPath+"//uploads//"+handler.Filename, "sha256")
+
+	fmt.Println(md5Hash)
+	fmt.Println(sha1Hash)
+	fmt.Println(sha256Hash)
+
 	out, err := exec.Command("olevba", "-j", "--reveal", exPath+"//uploads//"+handler.Filename).Output()
 	if err != nil {
 		fmt.Fprintf(w, "Failed to Upload File: \n")
@@ -80,9 +93,68 @@ func loadFile(fileName string) http.HandlerFunc {
 	}
 }
 
+func hash_file(filePath string, hashType string) (string, error) {
+
+	var hashString string
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return hashString, err
+	}
+
+	defer file.Close()
+
+	if hashType == "md5" {
+		hash := md5.New()
+
+		if _, err := io.Copy(hash, file); err != nil {
+			return hashString, err
+		}
+
+		hashInBytes := hash.Sum(nil)[:16]
+
+		hashString = hex.EncodeToString(hashInBytes)
+
+		return hashString, nil
+
+	} else if hashType == "sha1" {
+
+		hash := sha1.New()
+
+		if _, err := io.Copy(hash, file); err != nil {
+			return hashString, err
+		}
+
+		hashInBytes := hash.Sum(nil)[:20]
+
+		hashString = hex.EncodeToString(hashInBytes)
+
+		return hashString, nil
+
+	} else if hashType == "sha256" {
+
+		hash := sha256.New()
+
+		if _, err := io.Copy(hash, file); err != nil {
+			return hashString, err
+		}
+
+		hashInBytes := hash.Sum(nil)[:32]
+
+		hashString = hex.EncodeToString(hashInBytes)
+
+		return hashString, nil
+
+	} else {
+		return "Error: Unable to hash file.", err
+	}
+
+}
+
 func setupRoutes() {
 	http.HandleFunc("/", loadFile("saf"))
 	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/hash", uploadFile)
 	http.ListenAndServe(":8080", nil)
 }
 
